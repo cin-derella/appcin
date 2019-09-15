220960a9b3dc9943ee84e179cef71b5b5aa0b080
Shirley <shirley_cinderella@outlook.com>
2019-09-15 22:24:44 +0800

    travel

---
+++ appcin/Views/BorderButtton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderButtton.swift
+//  appcin
+//
+//  Created by Dante on 2019/9/15.
+//  Copyright © 2019 cinderella. All rights reserved.
+//
+
+import UIKit
+
+class BorderButtton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
